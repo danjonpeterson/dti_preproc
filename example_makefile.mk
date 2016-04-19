04e060c69978d202a32fba46eb0c2e6d71836731
Dan Peterson <djpeters@uw.edu>
2016-04-19 10:16:44 -0700

    updated example makefile for recent changes

--- example_makefile.mk
+++ example_makefile.mk
@@ -16,15 +16,13 @@
 # acqparams.txt
 # index.txt
 
+#CHANGE THIS FOR YOUR PROJECT
+PROJECT_HOME= /var/local/scratch/dti_preproc_dev
+
 cwd = $(shell pwd)
 SUBJECT=$(notdir $(cwd))
-PROJECT_HOME=/var/local/scratch/mclaughlin_test
 BINDIR=$(PROJECT_HOME)/bin
-#TOPUP_CONFIG=$(PROJECT_HOME)/lib/b02b0_accurate.cnf #can change to b02b0_accurate.cnf after testing
-TOPUP_CONFIG=b02b0.cnf
 OUTDIR=out
-EDDY_ITERATIONS=5 #can change to 5 after testing
-S0_COUNT=6
 
 SDC_METHOD = $(shell if [ -f fieldmap_phase.nii.gz ] ; then echo FUGUE; \
                     elif [ -f acqparams.txt ] ; then echo TOPUP; \
@@ -55,21 +53,21 @@ ifeq ($(SDC_METHOD),TOPUP)
 
 # unwarping using topup
 ${OUTDIR}/topup_out_fieldcoef.nii.gz: raw_diffusion.nii.gz bval.txt bvec.txt native_brain_mask.nii.gz acqparams.txt
-	${BINDIR}/dti_preproc/unwarp_bupbdown.sh -k raw_diffusion.nii.gz -b bval.txt -a acqparams.txt -M native_brain_mask.nii.gz -c ${TOPUP_CONFIG} -o ${OUTDIR} -n ${S0_COUNT}
+	${BINDIR}/dti_preproc/unwarp_bupbdown.sh -k raw_diffusion.nii.gz -b bval.txt -a acqparams.txt -M native_brain_mask.nii.gz -o ${OUTDIR} 
 
 # motion correction from topup output
 ${OUTDIR}/mc_unwarped_raw_diffusion.nii.gz: raw_diffusion.nii.gz bval.txt bvec.txt native_brain_mask.nii.gz acqparams.txt index.txt ${OUTDIR}/topup_out_fieldcoef.nii.gz
-	${BINDIR}/dti_preproc/motion_correct.sh -k raw_diffusion.nii.gz -b bval.txt -r bvec.txt -M native_brain_mask.nii.gz -m eddy_with_topup -i index.txt -a acqparams.txt -t ${OUTDIR}/topup_out -o ${OUTDIR} -n ${EDDY_ITERATIONS}
+	${BINDIR}/dti_preproc/motion_correct.sh -k raw_diffusion.nii.gz -b bval.txt -r bvec.txt -M native_brain_mask.nii.gz -m eddy_with_topup -i index.txt -a acqparams.txt -t ${OUTDIR}/topup_out -o ${OUTDIR}
 
 else ifeq ($(SDC_METHOD),FUGUE)
 
 # do just motion correction using eddy
 ${OUTDIR}/mc_raw_diffusion.nii.gz: raw_diffusion.nii.gz bval.txt bvec.txt native_brain_mask.nii.gz 
-	${BINDIR}/dti_preproc/motion_correct.sh -k raw_diffusion.nii.gz  -b bval.txt -r bvec.txt -M native_brain_mask.nii.gz -m eddy -o ${OUTDIR} -n ${EDDY_ITERATIONS}
+	${BINDIR}/dti_preproc/motion_correct.sh -k raw_diffusion.nii.gz  -b bval.txt -r bvec.txt -M native_brain_mask.nii.gz -m eddy -o ${OUTDIR} 
 
 # unwarp a motion corrected dataset with fugue
 ${OUTDIR}/mc_unwarped_raw_diffusion.nii.gz: ${OUTDIR}/mc_raw_diffusion.nii.gz fieldmap_phase.nii.gz fieldmap_magnitude.nii.gz native_brain_mask.nii.gz fieldmap_magnitude_brain_mask.nii.gz
-	${BINDIR}/dti_preproc/unwarp_fieldmap.sh -k ${OUTDIR}/mc_raw_diffusion.nii.gz -f fieldmap_phase.nii.gz -m fieldmap_magnitude.nii.gz -M native_brain_mask -o ${OUTDIR} -s -p fieldmap_magnitude_brain_mask.nii.gz &&\
+	${BINDIR}/dti_preproc/unwarp_fieldmap.sh -k ${OUTDIR}/mc_raw_diffusion.nii.gz -f fieldmap_phase.nii.gz -m fieldmap_magnitude.nii.gz -M native_brain_mask -o ${OUTDIR} -p fieldmap_magnitude_brain_mask.nii.gz -t 0.7482 -e 93.46 &&\
 	mv ${OUTDIR}/unwarped_mc_raw_diffusion.nii.gz ${OUTDIR}/mc_unwarped_raw_diffusion.nii.gz 
 
 else
@@ -78,7 +76,7 @@ endif
 
 # fit the tensor
 ${OUTDIR}/fa.nii.gz: ${OUTDIR}/mc_unwarped_raw_diffusion.nii.gz bval.txt 
-	${BINDIR}/dti_preproc/fit_tensor.sh -k ${OUTDIR}/mc_unwarped_raw_diffusion.nii.gz -b bval.txt -r ${OUTDIR}/bvec_mc.txt -m ${OUTDIR}/unwarped_brain_mask.nii.gz -o ${OUTDIR}
+	${BINDIR}/dti_preproc/fit_tensor.sh -k ${OUTDIR}/mc_unwarped_raw_diffusion.nii.gz -b bval.txt -r ${OUTDIR}/bvec_mc.txt -M ${OUTDIR}/unwarped_brain_mask.nii.gz -o ${OUTDIR}
 
 #TODO: take ${OUTDIR} values safely
 clean: 
